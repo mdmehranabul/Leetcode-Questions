0b57fece96ca48371317734f2dee77bd79d7344f
mdmehranabul <mehranabul@gmail.com>
2024-12-02 12:07:38 +0530

    Time: 3 ms (24.63%), Space: 17.6 MB (5.27%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap={")":"(","}":"{","]":"["}
+        stack=[]
+
+        for c in s:
+            if c in hashmap:
+                if stack and stack[-1]==hashmap[c]:
+                    stack.pop()
+                else: return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
+
+# Time Complexity : O(n)
+# Space Complexity : O(n)
+
+        
