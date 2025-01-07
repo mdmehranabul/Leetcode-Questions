6683670f077cc3b3d830d00962e7be9f5354a030
mdmehranabul <mehranabul@gmail.com>
2025-01-07 18:08:28 +0530

    Time: 78 ms (82.83%), Space: 21.1 MB (48.04%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        res=[]
+        def dfs(root):
+
+            if not root:
+                res.append("N")
+                return
+            
+            res.append(str(root.val))
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return ",".join(res)
+        
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        self.i=0
+        val=data.split(",")
+
+        def dfs():
+
+            if val[self.i]=="N": 
+                self.i+=1
+                return None
+            
+            root=TreeNode(int(val[self.i]))
+            self.i+=1
+            root.left=dfs()
+            root.right=dfs()
+            return root
+        return dfs()
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
+
+# Time Complexity - O(n)
+# Space Complexity - O(n)
