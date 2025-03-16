ef1c28f4292b5751cdcdd2e61c3694a7338a7757
mdmehranabul <mehranabul@gmail.com>
2025-03-17 01:29:39 +0530

    Time: 4489 ms (46.65%), Space: 18 MB (39.81%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,25 +1,28 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        row,col=len(board),len(board[0])
+        ROWS,COLS=len(board),len(board[0])
         path=set()
 
-        def dfs(r,c,i):
+        def backtrack(r,c,i):
+
             if i==len(word): return True
 
-            if (r<0 or c<0 or r>=row or c>=col or word[i]!=board[r][c] or (r,c) in path): return False
-
+            if (r<0 or c<0 or r>=ROWS or c>=COLS or board[r][c]!=word[i] or (r,c) in path):
+                return False
+            
             path.add((r,c))
-
-            res= dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
-
+            res= (backtrack(r+1,c,i+1) or 
+                backtrack(r-1,c,i+1) or 
+                backtrack(r,c-1,i+1) or 
+                backtrack(r,c+1,i+1))
+            
             path.remove((r,c))
-
             return res
         
-        for r in range(row):
-            for c in range(col):
-                if dfs(r,c,0): return True
+        for r in range(ROWS):
+            for c in range(COLS):
+                if backtrack(r,c,0): return True
         return False
+
+
         
-# Time Complexity - O(m x n x 4^l)
-# Time Complexity - O(l) ; l is the length of the word and m, n are dimensions of the board
