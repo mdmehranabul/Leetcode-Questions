3f906b4b123af6bbc7453bb880aef558a8b3900c
mdmehranabul <mehranabul@gmail.com>
2025-03-16 14:53:16 +0530

    Time: 7 ms (18.16%), Space: 18 MB (60.03%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,30 +1,32 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        A=nums1
-        B=nums2
-        tot=len(A)+len(B)
-        half=int(tot/2)
+        tot_len=len(nums1)+len(nums2)
+        half=tot_len//2
 
-        if len(A)>len(B): A,B=B,A
-
-        l,r=0,len(A)-1
+        if len(nums1)>len(nums2):
+            nums1,nums2=nums2,nums1
+        
+        l,r=0,len(nums1)-1
 
         while True:
             i=(l+r)//2
             j=half-i-2
 
-            Aleft=A[i] if i>=0 else float("-infinity")
-            Aright= A[i+1] if i+1<len(A) else float("infinity")
-            Bleft=B[j] if j>=0 else float("-infinity")
-            Bright= B[j+1] if j+1<len(B) else float("infinity")
+            Aleft=nums1[i] if i>=0 else float("-inf")
+            Aright=nums1[i+1] if i+1<len(nums1) else float("inf")
+            Bleft=nums2[j] if j>=0 else float("-inf")
+            Bright=nums2[j+1] if j+1<len(nums2) else float("inf")
 
             if Aleft<=Bright and Bleft<=Aright:
-                if tot%2:
+                if tot_len%2:
                     return min(Aright,Bright)
-                else: return (max(Aleft,Bleft)+min(Aright,Bright))/2
+                else:
+                    return (max(Aleft,Bleft)+min(Aright,Bright))/2
+            
             elif Aleft>Bright:
                 r=i-1
+            
             else:
                 l=i+1
-# Time Complexity - O(log(min(m,n)))
-# Space Complexity - O(1)
+
+        
