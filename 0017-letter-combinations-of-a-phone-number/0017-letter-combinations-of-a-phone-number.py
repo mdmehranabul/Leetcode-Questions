cbc7537db07e2d5cc21201bcecf95495636e0548
mdmehranabul <mehranabul@gmail.com>
2025-03-17 16:59:39 +0530

    Time: 0 ms (100%), Space: 17.7 MB (92.08%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res,curset=[],[]
-        mapping={
+        subset,curr=[],[]
+        hashmap={
             "2":"abc",
             "3":"def",
             "4":"ghi",
@@ -12,21 +12,18 @@ class Solution:
             "9":"wxyz"
         }
 
-        def dfs(i):
+        def backtrack(i):
             if i==len(digits):
-                res.append("".join(curset.copy()))
+                subset.append("".join(curr))
                 return
             
-            for c in mapping[digits[i]]:
-                curset.append(c)
-                dfs(i+1)
-                curset.pop()
+            for c in hashmap[digits[i]]:
+                curr.append(c)
+                backtrack(i+1)
+                curr.pop()
             
-        if digits:
-            dfs(0)
-            return res
-        else:
-            return []
+            return subset
+        
+        if digits: backtrack(0)
 
-# Time Complexity - O(n x 4^n)
-# Space Complexity - O(n)
+        return subset
