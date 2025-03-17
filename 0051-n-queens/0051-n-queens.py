404db449dc1d780175dcd33f81795e4243415c34
mdmehranabul <mehranabul@gmail.com>
2025-03-17 17:04:44 +0530

    Time: 11 ms (74.61%), Space: 18.3 MB (56.1%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,35 +1,32 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        curset=[["."] * n for i in range(n)]
-        res=[]
+        subset,curr=[],[]
         col=set()
         posdiag=set()
         negdiag=set()
+        board=[['.']*n for i in range(n)]
 
-        def dfs(r):
+        def backtrack(r):
+
             if r==n:
-                
-                res.append(["".join(row) for row in curset])
+                copy=["".join(row) for row in board]
+                subset.append(copy)
                 return
             
+
             for c in range(n):
                 if c in col or (r+c) in posdiag or (r-c) in negdiag:
                     continue
                 
-                curset[r][c]="Q"
                 col.add(c)
-                negdiag.add(r-c)
                 posdiag.add(r+c)
+                negdiag.add(r-c)
 
-                dfs(r+1)
-                curset[r][c]="."
+                board[r][c]="Q"
+                backtrack(r+1)
                 col.remove(c)
-                negdiag.remove(r-c)
                 posdiag.remove(r+c)
-                #print(curset)
-        dfs(0)
-        return res
-
-            
-# Time Complexity - O(n!)
-# Space Complexity - O(n^2)
+                negdiag.remove(r-c)
+                board[r][c]="."
+        backtrack(0)
+        return subset
