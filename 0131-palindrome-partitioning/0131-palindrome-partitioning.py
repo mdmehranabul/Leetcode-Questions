d547d356295e6bb75cc23e7e9b592d011f44a1b2
mdmehranabul <mehranabul@gmail.com>
2025-03-17 16:30:30 +0530

    Time: 53 ms (44.44%), Space: 34.7 MB (19.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,25 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res,curset=[],[]
+        res,curr=[],[]
 
-        def dfs(i):
-
+        def backtrack(i):
             if i==len(s):
-                res.append(curset.copy())
+                res.append(curr.copy())
+                return
             
             for j in range(i,len(s)):
-                if self.ispalindrome(s[i:j+1]):
-                    curset.append(s[i:j+1])
-                    dfs(j+1)
-                    curset.pop()
-                
-        
-        dfs(0)
+                if self.palin(s[i:j+1]):
+                    curr.append(s[i:j+1])
+                    backtrack(j+1)
+                    curr.pop()
+            
+            return res
+        backtrack(0)
         return res
 
-    def ispalindrome(self,s):
-        if s==s[::-1] : return True
-        else: return False
-
-# Time Complexity - O(n * 2^n)
-# Space Complexity - O(n)
+    def palin(self,s):
+        return s==s[::-1]
+        
