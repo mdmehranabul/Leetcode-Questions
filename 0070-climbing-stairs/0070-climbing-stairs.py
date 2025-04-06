cf592fbbcee50c7a53ecfe5509dc760836139b27
mdmehranabul <mehranabul@gmail.com>
2025-04-06 10:43:45 +0530

    Time: 0 ms (100%), Space: 17.6 MB (95.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,43 +1,39 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # def recur(i):
-        #     if i==n: return 1
+        # def dfs(i):
         #     if i>n: return 0
+        #     if i==n: return 1
 
-        #     return recur(i+1) + recur(i+2)
-        
-        # return recur(0)
-
+        #     return dfs(i+1)+dfs(i+2)
+        # return dfs(0)
         # cache=[-1]*(n+1)
-
-        # def recur(i,cache):
+        # def dfs(i,cache):
         #     if i>n: return 0
-        #     if cache[i]!=-1:
-        #         return cache[i]
-            
         #     if i==n: return 1
-            
 
-        #     cache[i]=recur(i+1,cache)+recur(i+2,cache)
+        #     if cache[i]!=-1: return cache[i]
+
+        #     cache[i]=dfs(i+1,cache)+dfs(i+2,cache)
 
         #     return cache[i]
+        # return dfs(0,cache)
+
+        # if n<=2: return n
+
+        # dp=[0]*(n+1)
+
+        # dp[1],dp[2]=1,2
+
+        # for i in range(3,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
         
-        # return recur(0,cache)
+        # return dp[n]
 
         if n<=2: return n
-        dp=[0]*(n+1)
 
-        dp[1],dp[2]=1,2
+        a,b=1,2
 
-        for i in range(3,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        
-        return dp[n]
+        for i in range(n-2):
+            a,b=b,a+b
+        return b
 
-
-    
-
-
-
-
-        
