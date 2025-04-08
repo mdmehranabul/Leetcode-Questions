dab1b6d990b917302cb47cbeefd97610e5caf760
mdmehranabul <mehranabul@gmail.com>
2025-04-08 18:50:40 +0530

    Time: 130 ms (64.21%), Space: 17.9 MB (35.11%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -4,15 +4,17 @@ class Solution:
 
         for i in range(len(s)):
             l,r=i,i
+
             while l>=0 and r<len(s) and s[r]==s[l]:
                 res+=1
                 l-=1
                 r+=1
+            
             l,r=i,i+1
+
             while l>=0 and r<len(s) and s[r]==s[l]:
                 res+=1
                 l-=1
                 r+=1
         return res
-            
         
