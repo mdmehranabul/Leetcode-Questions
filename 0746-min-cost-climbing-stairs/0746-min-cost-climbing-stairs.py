44b3826de73264a14787ba939c5cabf3eada1d9c
mdmehranabul <mehranabul@gmail.com>
2025-04-08 16:57:47 +0530

    Time: 8 ms (10.52%), Space: 19 MB (17.23%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,31 +1,26 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-         
+
         # def dfs(i):
-        #     if i>=len(cost): return 0
-
-        #     return cost[i] + min(dfs(i+1),dfs(i+2))
-        # return min(dfs(0),dfs(1))
-
-        # cache=[-1]*(len(cost)+1)
-        # def dfs(i,cache):
-        #     if i>=len(cost): return 0
-
-        #     if cache[i]!=-1: return cache[i]
-        #     cache[i]=cost[i]+min(dfs(i+1,cache),dfs(i+2,cache))
-        #     return cache[i]
-        # return min(dfs(0,cache),dfs(1,cache))
-
-        # dp=[0]*(len(cost)+2)
-
-        # for i in range(len(cost)-1,-1,-1):
-        #     dp[i]=cost[i]+min(dp[i+1],dp[i+2])
+        #     if i>=len(cost):
+        #         return 0
+        #     return cost[i]+min(dfs(i+1),dfs(i+2))
         
-        # return min(dp[0],dp[1])
+        # return min(dfs(0),dfs(1))
+        n=len(cost)
+        cache=[-1]*(n+1)
+
+        def dfs(i):
+            if i>=len(cost):
+                return 0
+            if cache[i]!=-1: return cache[i]
+            cache[i]=cost[i]+min(dfs(i+1),dfs(i+2))
+            return cache[i]
+            
+        return min(dfs(0),dfs(1))
 
 
-        for i in range(len(cost)-3,-1,-1):
-            cost[i]=cost[i]+min(cost[i+1],cost[i+2])
-        return min(cost[0],cost[1])
+            
 
-
+            
+        
