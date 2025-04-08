08a9da4917d38f361e8b5815fdadb03d5f5371b8
mdmehranabul <mehranabul@gmail.com>
2025-04-08 16:53:48 +0530

    Time: 3 ms (73.64%), Space: 18.1 MB (29.96%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,43 +1,31 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-
+         
         # def dfs(i):
         #     if i>=len(cost): return 0
 
         #     return cost[i] + min(dfs(i+1),dfs(i+2))
-        
         # return min(dfs(0),dfs(1))
-# Time Complexity : O(2^n)
-# Space Complexity : O(n)
 
         # cache=[-1]*(len(cost)+1)
-
-        # def dfs(i):
+        # def dfs(i,cache):
         #     if i>=len(cost): return 0
+
         #     if cache[i]!=-1: return cache[i]
-
-        #     cache[i]=cost[i]+ min(dfs(i+1),dfs(i+2))
-
+        #     cache[i]=cost[i]+min(dfs(i+1,cache),dfs(i+2,cache))
         #     return cache[i]
-        
-        # return min(dfs(0),dfs(1))
+        # return min(dfs(0,cache),dfs(1,cache))
 
-# Time Complexity : O(n)
-# Space Complexity : O(n)
-        dp=[0]*(len(cost)+2)
+        # dp=[0]*(len(cost)+2)
+
+        # for i in range(len(cost)-1,-1,-1):
+        #     dp[i]=cost[i]+min(dp[i+1],dp[i+2])
         
-        #for i in range(len(cost) - 1, -1, -1):
-        #    dp[i] = cost[i] + min(dp[i + 1], dp[i + 2])
-        #return min(dp[0],dp[1])
-# Time Complexity : O(n)
-# Space Complexity : O(n)
+        # return min(dp[0],dp[1])
+
 
         for i in range(len(cost)-3,-1,-1):
-            cost[i]+=min(cost[i+1],cost[i+2])
+            cost[i]=cost[i]+min(cost[i+1],cost[i+2])
         return min(cost[0],cost[1])
 
-# Time Complexity : O(n)
-# Space Complexity : O(1)
 
-
-        
