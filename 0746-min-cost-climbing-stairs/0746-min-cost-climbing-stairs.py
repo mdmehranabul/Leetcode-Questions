01e73353dc1b8482fd8d9495d88f06a9951674b6
mdmehranabul <mehranabul@gmail.com>
2025-04-08 17:12:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (87.07%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -7,17 +7,27 @@ class Solution:
         #     return cost[i]+min(dfs(i+1),dfs(i+2))
         
         # return min(dfs(0),dfs(1))
-        n=len(cost)
-        cache=[-1]*(n+1)
+        # n=len(cost)
+        # cache=[-1]*(n+1)
 
-        def dfs(i):
-            if i>=len(cost):
-                return 0
-            if cache[i]!=-1: return cache[i]
-            cache[i]=cost[i]+min(dfs(i+1),dfs(i+2))
-            return cache[i]
+        # def dfs(i):
+        #     if i>=len(cost):
+        #         return 0
+        #     if cache[i]!=-1: return cache[i]
+        #     cache[i]=cost[i]+min(dfs(i+1),dfs(i+2))
+        #     return cache[i]
             
-        return min(dfs(0),dfs(1))
+        # return min(dfs(0),dfs(1))
+
+        # dp=[0]*(n+2)
+
+        # for i in range(len(cost)-1,-1,-1):
+        #     dp[i]=cost[i]+min(dp[i+1],dp[i+2])
+        # return min(dp[0],dp[1])
+
+        for i in range(len(cost)-3,-1,-1):
+            cost[i]=cost[i]+min(cost[i+1],cost[i+2])
+        return min(cost[0],cost[1])
 
 
             
