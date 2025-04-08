1093ef11411248bd9165fd39cac5ab12ac60feff
mdmehranabul <mehranabul@gmail.com>
2025-04-08 18:25:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -9,14 +9,26 @@ class Solution:
         #     return max(arr[i]+dfs(i+2,arr),dfs(i+1,arr))
         # return max(dfs(0,nums[1:]),dfs(0,nums[:-1]))
 
-        if len(nums)==1: return nums[0]
-        n=len(nums)
-        #cache=[-1]*(n+1)
-        def dfs(i,arr,cache):
-            if i>=len(arr):
-                return 0
-            if cache[i]!=-1: return cache[i]
-            cache[i]= max(arr[i]+dfs(i+2,arr,cache),dfs(i+1,arr,cache))
-            return cache[i]
-        return max(dfs(0,nums[1:],[-1]*(n+1)),dfs(0,nums[:-1],[-1]*(n+1)))
+        # if len(nums)==1: return nums[0]
+        # n=len(nums)
+        # #cache=[-1]*(n+1)
+        # def dfs(i,arr,cache):
+        #     if i>=len(arr):
+        #         return 0
+        #     if cache[i]!=-1: return cache[i]
+        #     cache[i]= max(arr[i]+dfs(i+2,arr,cache),dfs(i+1,arr,cache))
+        #     return cache[i]
+        # return max(dfs(0,nums[1:],[-1]*(n+1)),dfs(0,nums[:-1],[-1]*(n+1)))
         
+        def dfs(nums):
+            if len(nums)==0: return 0
+            if len(nums)==1: return nums[0]
+
+            rob1,rob2=nums[0],max(nums[0],nums[1])
+
+            for i in range(2,len(nums)):
+                rob1,rob2=rob2,max(rob2,rob1+nums[i])
+            
+            return rob2
+        
+        return max(dfs(nums[1:]),dfs(nums[:-1]))
